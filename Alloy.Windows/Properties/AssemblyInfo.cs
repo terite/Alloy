080babb10fd4b2ce7267ca913bc4ace3514f386b
ermau <me@ermau.com>
2011-10-21 18:44:02 -0400

    Add windows library

---
+++ Alloy.Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Alloy.Windows")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Eric Maupin and David Stensland")]
+[assembly: AssemblyProduct ("Alloy")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin and David Stensland 2011")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("df86759e-deea-49a5-a638-ec19e9b39cfc")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
