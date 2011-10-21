553b362cfecda7472c276397dc2c4d1e5ee243fa
ermau <me@ermau.com>
2011-10-21 18:01:09 -0400

    Add Project

---
+++ Alloy/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Alloy")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Eric Maupin and David Stensland")]
+[assembly: AssemblyProduct ("Alloy")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin and David Stensland 2011")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7b19d3a4-f708-45b2-aa8e-6ed5b84d5de6")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
