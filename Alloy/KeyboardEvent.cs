5af73a5b4798e4045d99e0a572bcf2d8aac840d1
ermau <me@ermau.com>
2012-01-04 20:29:29 -0500

    Events

---
+++ Alloy/KeyboardEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tempest;
+
+namespace Alloy
+{
+	public class KeyboardEvent
+	{
+	}
+
+	public class KeyboardEventSerializer
+		: ISerializer<KeyboardEvent>
+	{
+		public static readonly KeyboardEventSerializer Instance = new KeyboardEventSerializer();
+
+		public void Serialize (ISerializationContext context, IValueWriter writer, KeyboardEvent element)
+		{
+			throw new NotImplementedException();
+		}
+
+		public KeyboardEvent Deserialize (ISerializationContext context, IValueReader reader)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Alloy/MouseEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tempest;
+
+namespace Alloy
+{
+	public class MouseEvent
+	{
+	}
+
+	public class MouseEventSerializer
+		: ISerializer<MouseEvent>
+	{
+		public static readonly MouseEventSerializer Instance = new MouseEventSerializer();
+
+		public void Serialize (ISerializationContext context, IValueWriter writer, MouseEvent element)
+		{
+			throw new NotImplementedException();
+		}
+
+		public MouseEvent Deserialize (ISerializationContext context, IValueReader reader)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
