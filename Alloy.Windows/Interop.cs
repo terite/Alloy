c597f35bc941ed682c51f2b6162d7dabe179487f
ermau <me@ermau.com>
2012-01-15 19:28:02 -0500

    Add simulate event interops

--- Alloy.Windows/Interop.cs
+++ Alloy.Windows/Interop.cs
@@ -12,6 +12,49 @@ namespace Alloy.Windows
 		public int Bottom;
 	}
 
+	[StructLayout (LayoutKind.Sequential)]
+	internal struct KEYBDINPUT
+	{
+		public ushort wVk;
+		public ushort wScan;
+		public uint dwFlags;
+		public uint time;
+		public IntPtr dwExtraInfo;
+	}
+
+	[StructLayout (LayoutKind.Sequential)]
+	struct MOUSEINPUT
+	{
+		int dx;
+		int dy;
+		int mouseData;
+		int dwFlags;
+		int time;
+		IntPtr dwExtraInfo;
+	}
+
+	[StructLayout (LayoutKind.Sequential)]
+	struct HARDWAREINPUT
+	{
+		public int uMsg;
+		public short wParamL;
+		public short wParamH;
+	}
+
+	[StructLayout (LayoutKind.Explicit)]
+	internal struct InputUnion
+	{
+		[FieldOffset(0)] public MOUSEINPUT mi;
+		[FieldOffset(0)] public KEYBDINPUT ki;
+		[FieldOffset(0)] public HARDWAREINPUT hi;
+	}
+
+	internal struct INPUT
+	{
+		public int type;
+		public InputUnion u;
+	}
+
 	internal delegate int HookProc (int nCode, int wParam, int lParam);
 
 	internal static class Interop
@@ -36,5 +79,20 @@ namespace Alloy.Windows
 
 		[DllImport ("user32.dll")]
 		public static extern bool UnhookWindowsHookEx (int idHook);
+
+		[DllImport ("user32.dll")]
+		public static extern uint SendInput (uint nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+
+		[DllImport ("user32.dll")]
+		public static extern extern uint MapVirtualKey (uint uCode, VirtualMapType uMapType);
+	}
+
+	internal enum VirtualMapType
+		: uint
+	{
+		VirtualKeyToScanCode = 0,
+		ScanCodeToVirtualKey = 1,
+		VirtualKeyToChar = 2,
+		ScanCodeToVirtualKeyEx = 3,
 	}
 }
